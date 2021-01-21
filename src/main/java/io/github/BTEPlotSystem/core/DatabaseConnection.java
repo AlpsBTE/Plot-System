1a181ffa32bb68960fb57897434c3f55182733ef
R3tuxn <jonas.limpl@hak-bregenz.at>
2021-01-21 16:48:53 +0100

    Implemented DatabaseConnection.java

---
+++ src/main/java/io/github/BTEPlotSystem/core/DatabaseConnection.java
@@ -0,0 +1,38 @@
+package github.BTEPlotSystem.core;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+
+public class DatabaseConnection {
+
+    private static Connection connection;
+
+    public DatabaseConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("MySQL JDBC Driver Registered!");
+
+            try {
+                FileConfiguration config = github.BTEPlotSystem.BTEPlotSystem.getPlugin().getConfig();
+                connection = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/alpsbte",
+                        config.getString("review-system.database.username"),
+                        config.getString("review-system.database.password"));
+
+                Bukkit.getLogger().log(Level.INFO, "SQL Connection to database established!");
+            } catch (SQLException ex) {
+                Bukkit.getLogger().log(Level.SEVERE, "Connection Failed!", ex);
+            }
+        } catch (Exception ex) {
+            Bukkit.getLogger().log(Level.SEVERE, "MySQL JDBC Driver not found!", ex);
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+}
