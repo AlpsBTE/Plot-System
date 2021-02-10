9dd9d0aabd7aca98cb05b7445e1688db1e081b60
R3tuxn <jonas.limpl@hak-bregenz.at>
2021-02-10 18:03:47 +0100

    Added spawn point to the config

--- src/main/java/io/github/BTEPlotSystem/utils/Utils.java
+++ src/main/java/io/github/BTEPlotSystem/utils/Utils.java
@@ -1,7 +1,20 @@
 package github.BTEPlotSystem.utils;
 
-import me.arcaniax.hdb.api.HeadDatabaseAPI;
+import github.BTEPlotSystem.BTEPlotSystem;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
 
 public class Utils {
-    public static HeadDatabaseAPI headDatabaseAPI;
+    public static Location getSpawnPoint() {
+        FileConfiguration config = BTEPlotSystem.getPlugin().getConfig();
+
+        return new Location(Bukkit.getWorld("world"),
+                config.getDouble("spawn-point.x"),
+                config.getDouble("spawn-point.y"),
+                config.getDouble("spawn-point.z"),
+                (float) config.getDouble("spawn-point.yaw"),
+                (float) config.getDouble("spawn-point.pitch")
+        );
+    }
 }
