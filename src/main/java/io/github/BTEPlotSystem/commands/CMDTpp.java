b0753d3f4c008f324b75a3e7cbd644c6b3eb2589
R3tuxn <jonas.limpl@hak-bregenz.at>
2021-02-28 17:26:16 +0100

    Added command /tpp <Player>

---
+++ src/main/java/io/github/BTEPlotSystem/commands/CMDTpp.java
@@ -0,0 +1,27 @@
+package github.BTEPlotSystem.commands;
+
+import github.BTEPlotSystem.core.menus.CompanionMenu;
+import github.BTEPlotSystem.utils.Utils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CMDTpp implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
+        if(sender instanceof Player) {
+            if(sender.hasPermission("alpsbte.tpp")) {
+                Player player = (Player) sender;
+                try {
+                    Player targetPlayer = player.getServer().getPlayer(args[0]);
+                    player.teleport(targetPlayer);
+                    player.sendMessage(Utils.getInfoMessageFormat("Teleporting to player..."));
+                } catch (Exception ignore) {
+                    player.sendMessage(Utils.getErrorMessageFormat("Usage: /tpp <Player>"));
+                }
+            }
+        }
+        return true;
+    }
+}
