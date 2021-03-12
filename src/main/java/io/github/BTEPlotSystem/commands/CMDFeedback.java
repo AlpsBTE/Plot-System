5535c2f89b9cb034cd37be3326f7e2e30326a83c
R3tuxn <jonas.limpl@hak-bregenz.at>
2021-03-12 22:37:37 +0100

    Improved feedback command

--- src/main/java/io/github/BTEPlotSystem/commands/CMDFeedback.java
+++ src/main/java/io/github/BTEPlotSystem/commands/CMDFeedback.java
@@ -2,6 +2,7 @@ package github.BTEPlotSystem.commands;
 
 import github.BTEPlotSystem.core.DatabaseConnection;
 import github.BTEPlotSystem.core.menus.FeedbackMenu;
+import github.BTEPlotSystem.core.plots.Plot;
 import github.BTEPlotSystem.core.plots.PlotManager;
 import github.BTEPlotSystem.utils.Utils;
 import org.bukkit.Bukkit;
@@ -20,24 +21,34 @@ public class CMDFeedback implements CommandExecutor {
         if(sender instanceof Player) {
             if(args.length == 1) {
                 if(Utils.TryParseInt(args[0]) != null) {
-                    int ID = Integer.parseInt(args[0]);
+                    try {
+                        Plot plot = new Plot(Integer.parseInt(args[0]));
 
-                    if(PlotManager.plotExists(ID)) {
-                        try {
-                            ResultSet rs = DatabaseConnection.createStatement().executeQuery("SELECT idreview FROM plots WHERE idplot = '" + ID + "'");
-                            rs.next();
+                        if(PlotManager.plotExists(plot.getID())) {
+                            if(plot.isReviewed() || plot.wasRejected()) {
+                                if(plot.getBuilder().getUUID().equals(((Player) sender).getUniqueId()) || sender.hasPermission("alpsbte.review")) {
+                                    ResultSet rs = DatabaseConnection.createStatement().executeQuery("SELECT idreview FROM plots WHERE idplot = '" + plot.getID() + "'");
+                                    rs.next();
 
-                            new FeedbackMenu((Player) sender, rs.getInt(1));
-                        } catch (SQLException ex) {
-                            sender.sendMessage(Utils.getErrorMessageFormat("An error occurred! Please try again!"));
-                            Bukkit.getLogger().log(Level.SEVERE, "An SQL error occurred!", ex);
+                                    new FeedbackMenu((Player) sender, rs.getInt(1));
+                                } else {
+                                    sender.sendMessage(Utils.getErrorMessageFormat("You don't have permission to see this feedback!"));
+                                }
+                            } else {
+                                sender.sendMessage(Utils.getErrorMessageFormat("This plot has not yet been reviewed!"));
+                            }
+                        } else {
+                            sender.sendMessage(Utils.getErrorMessageFormat("Could not find plot with ID #" + plot.getID() + "!"));
                         }
-                    } else {
-                        sender.sendMessage(Utils.getErrorMessageFormat("Could not find plot with ID #" + ID + "!"));
+                    } catch (SQLException ex) {
+                        sender.sendMessage(Utils.getErrorMessageFormat("An error occurred! Please try again!"));
+                        Bukkit.getLogger().log(Level.SEVERE, "An SQL error occurred!", ex);
                     }
                 } else {
                     sender.sendMessage(Utils.getErrorMessageFormat("Please enter a valid ID!"));
                 }
+            } else {
+                sender.sendMessage(Utils.getErrorMessageFormat("§lUsage: §c/feedback <ID>"));
             }
         }
         return true;
