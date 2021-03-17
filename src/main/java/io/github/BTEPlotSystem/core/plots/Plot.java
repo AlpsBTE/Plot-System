a3d169d044fdeaa48007420f1a8d385a2b90352e
R3tuxn <jonas.limpl@hak-bregenz.at>
2021-03-17 13:22:57 +0100

    Redesigned plot class; Improved loading time of the plot UI's

--- src/main/java/io/github/BTEPlotSystem/core/plots/Plot.java
+++ src/main/java/io/github/BTEPlotSystem/core/plots/Plot.java
@@ -26,79 +26,85 @@ public class Plot extends PlotPermissions {
 
     private final int ID;
     private final CityProject cityProject;
-    private final Builder builder;
-    private double[] geoCoordinates;
     private final Difficulty difficulty;
-    private Review review;
 
     public Plot(int ID) throws SQLException {
         super(ID);
+
         this.ID = ID;
 
-        ResultSet rs = DatabaseConnection.createStatement().executeQuery("SELECT * FROM plots WHERE idplot = '" + getID() + "'");
+        ResultSet rs = DatabaseConnection.createStatement().executeQuery("SELECT idcity, iddifficulty FROM plots WHERE idplot = '" + getID() + "'");
         rs.next();
 
         // City ID
-        this.cityProject = new CityProject(rs.getInt("idcity"));
-
-        // Builder and Plot Coordinates
-        this.builder = (getStatus() != Status.unclaimed) ?
-                new Builder(UUID.fromString(rs.getString("uuidplayer"))) :
-                null;
+        this.cityProject = new CityProject(rs.getInt(1));
 
         // Set Plot Difficulty
-        this.difficulty = Difficulty.values()[rs.getInt("iddifficulty")];
-
-        // Set Review Class
-        if(getStatus() == Status.complete || wasRejected()) {
-            review = new Review(rs.getInt("idreview"));
-        }
-
-        // Player MC Coordinates
-        String[] mcLocation = rs.getString("mcCoordinates").split(",");
-
-       // Added support for the recently added Y plot coordinate
-        Vector mcCoordinates;
-        if(mcLocation.length == 2) {
-            mcCoordinates = new Vector(Double.parseDouble(mcLocation[0]),0,Double.parseDouble(mcLocation[1]));
-        } else {
-            mcCoordinates = new Vector(Double.parseDouble(mcLocation[0]),Double.parseDouble(mcLocation[1]),Double.parseDouble(mcLocation[2]));
-        }
-
-        // Convert MC coordinates to geo coordinates
-        try {
-            this.geoCoordinates = CoordinateConversion.convertToGeo(mcCoordinates.getX(), mcCoordinates.getZ());
-        } catch (OutOfProjectionBoundsException ex) {
-            Bukkit.getLogger().log(Level.SEVERE, "Could not convert MC coordinates to geo coordinates!", ex);
-        }
+        this.difficulty = Difficulty.values()[rs.getInt(2)];
     }
 
     public int getID() { return ID; }
 
     public CityProject getCity() { return cityProject; }
 
-    public Builder getBuilder() { return builder; }
-
     public Difficulty getDifficulty() { return difficulty; }
-
-    public Review getReview() { return review; }
 
     public File getSchematic() { return Paths.get(PlotManager.getSchematicPath(), String.valueOf(cityProject.getID()), getID() + ".schematic").toFile(); }
 
-    public String getGeoCoordinatesNumeric() { return CoordinateConversion.formatGeoCoordinatesNumeric(geoCoordinates); }
+    public Builder getBuilder() {
+        try {
+            if(getStatus() != Status.unclaimed) {
+                ResultSet rs = DatabaseConnection.createStatement().executeQuery("SELECT uuidplayer FROM plots WHERE idplot = '" + getID() + "'");
+                rs.next();
 
-    public Date getLastActivity() throws SQLException {
-        ResultSet rs = DatabaseConnection.createStatement().executeQuery("SELECT lastActivity FROM plots WHERE idplot = '" + getID() + "'");
-        rs.next();
-
-        return rs.getDate(1);
+                return new Builder(UUID.fromString(rs.getString(1)));
+            }
+        } catch (SQLException ex) {
+            Bukkit.getLogger().log(Level.SEVERE, "A SQL error occurred!", ex);
+        }
+        return null;
     }
 
-    public Status getStatus() throws SQLException {
-        ResultSet rs = DatabaseConnection.createStatement().executeQuery("SELECT status FROM plots WHERE idplot = '" + getID() + "'");
-        rs.next();
+    public Review getReview() {
+        try {
+            if(getStatus() == Status.complete || wasRejected()) {
+                ResultSet rs = DatabaseConnection.createStatement().executeQuery("SELECT idreview FROM plots WHERE idplot = '" + getID() + "'");
+                rs.next();
 
-        return Status.valueOf(rs.getString("status"));
+                return new Review(rs.getInt(1));
+            }
+        } catch (SQLException ex) {
+            Bukkit.getLogger().log(Level.SEVERE, "A SQL error occurred!", ex);
+        }
+        return null;
+    }
+
+    public String getGeoCoordinatesNumeric() {
+        try {
+            ResultSet rs = DatabaseConnection.createStatement().executeQuery("SELECT mcCoordinates FROM plots WHERE idplot = '" + getID() + "'");
+            rs.next();
+
+            // Player MC Coordinates
+            String[] mcLocation = rs.getString(1).split(",");
+
+            // Added support for the recently added Y plot coordinate
+            Vector mcCoordinates;
+            if(mcLocation.length == 2) {
+                mcCoordinates = new Vector(Double.parseDouble(mcLocation[0]),0,Double.parseDouble(mcLocation[1]));
+            } else {
+                mcCoordinates = new Vector(Double.parseDouble(mcLocation[0]),Double.parseDouble(mcLocation[1]),Double.parseDouble(mcLocation[2]));
+            }
+
+            // Convert MC coordinates to geo coordinates
+            try {
+                return CoordinateConversion.formatGeoCoordinatesNumeric(CoordinateConversion.convertToGeo(mcCoordinates.getX(), mcCoordinates.getZ()));
+            } catch (OutOfProjectionBoundsException ex) {
+                Bukkit.getLogger().log(Level.SEVERE, "Could not convert MC coordinates to geo coordinates!", ex);
+            }
+        } catch (SQLException ex) {
+            Bukkit.getLogger().log(Level.SEVERE, "A SQL error occurred!", ex);
+        }
+        return null;
     }
 
     public int getScore() throws SQLException {
@@ -108,6 +114,20 @@ public class Plot extends PlotPermissions {
             return rs.getInt("score");
         }
         return 0;
+    }
+
+    public Status getStatus() throws SQLException {
+        ResultSet rs = DatabaseConnection.createStatement().executeQuery("SELECT status FROM plots WHERE idplot = '" + getID() + "'");
+        rs.next();
+
+        return Status.valueOf(rs.getString("status"));
+    }
+
+    public Date getLastActivity() throws SQLException {
+        ResultSet rs = DatabaseConnection.createStatement().executeQuery("SELECT lastActivity FROM plots WHERE idplot = '" + getID() + "'");
+        rs.next();
+
+        return rs.getDate(1);
     }
 
     public Slot getSlot() throws SQLException {
@@ -125,17 +145,6 @@ public class Plot extends PlotPermissions {
         return null;
     }
 
-    public void setLastActivity(boolean setNull) throws SQLException {
-        PreparedStatement statement = DatabaseConnection.prepareStatement("UPDATE plots SET lastActivity = ? WHERE idplot = '" + getID() + "'");
-        statement.setDate(1, java.sql.Date.valueOf(java.time.LocalDate.now()));
-
-        if(setNull) {
-            statement = DatabaseConnection.prepareStatement("UPDATE plots SET lastActivity = DEFAULT(lastActivity) WHERE idplot = '" + getID() + "'");
-        }
-
-        statement.executeUpdate();
-    }
-
     public void setBuilder(String UUID) throws SQLException {
         PreparedStatement statement;
         if(UUID != null) {
@@ -147,18 +156,28 @@ public class Plot extends PlotPermissions {
         statement.executeUpdate();
     }
 
-    // Update plot status
+    public void setScore(int score) throws SQLException {
+        PreparedStatement statement = DatabaseConnection.prepareStatement("UPDATE plots SET score = ? WHERE idplot = '" + getID() + "'");
+        statement.setInt(1, score);
+        statement.executeUpdate();
+        BTEPlotSystem.getHolograms().stream().filter(holo -> holo.getHologramName().equals("ScoreLeaderboard")).findFirst().get().updateLeaderboard();
+    }
+
     public void setStatus(Status status) throws SQLException {
         PreparedStatement statement = DatabaseConnection.prepareStatement("UPDATE plots SET status = ? WHERE idplot = '" + getID() + "'");
         statement.setString(1, status.name());
         statement.executeUpdate();
     }
 
-    public void setScore(int score) throws SQLException {
-        PreparedStatement statement = DatabaseConnection.prepareStatement("UPDATE plots SET score = ? WHERE idplot = '" + getID() + "'");
-        statement.setInt(1, score);
+    public void setLastActivity(boolean setNull) throws SQLException {
+        PreparedStatement statement = DatabaseConnection.prepareStatement("UPDATE plots SET lastActivity = ? WHERE idplot = '" + getID() + "'");
+        statement.setDate(1, java.sql.Date.valueOf(java.time.LocalDate.now()));
+
+        if(setNull) {
+            statement = DatabaseConnection.prepareStatement("UPDATE plots SET lastActivity = DEFAULT(lastActivity) WHERE idplot = '" + getID() + "'");
+        }
+
         statement.executeUpdate();
-        BTEPlotSystem.getHolograms().stream().filter(holo -> holo.getHologramName().equals("ScoreLeaderboard")).findFirst().get().updateLeaderboard();
     }
 
     // Get Open Street Maps link
