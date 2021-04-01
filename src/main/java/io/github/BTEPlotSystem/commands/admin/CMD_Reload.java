65407ab150e804566f59d60d23b5248c1a391413
R3tuxn <jonas.limpl@hak-bregenz.at>
2021-04-01 15:18:28 +0200

    Reworked reload command

--- src/main/java/io/github/BTEPlotSystem/commands/admin/CMD_Reload.java
+++ src/main/java/io/github/BTEPlotSystem/commands/admin/CMD_Reload.java
@@ -7,7 +7,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import java.util.logging.Level;
 
@@ -17,23 +16,13 @@ public class CMD_Reload implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
         if (sender.hasPermission("alpsbte.admin")){
             try {
-                if(args.length == 1) {
-                    switch (args[0].toLowerCase()) {
-                        case "config":
-                            BTEPlotSystem.getPlugin().saveConfig();
-                            BTEPlotSystem.getPlugin().reloadConfig();
-                            sender.sendMessage(Utils.getInfoMessageFormat("Successfully reloaded config!"));
-                            break;
-                        case "holograms":
-                            BTEPlotSystem.getHolograms().forEach(HolographicDisplay::updateLeaderboard);
-                            sender.sendMessage(Utils.getInfoMessageFormat("Successfully reloaded holograms!"));
-                            break;
-                        default:
-                            sender.sendMessage(Utils.getErrorMessageFormat("§lUsage: §c/reload <config/holograms>"));
-                    }
-                } else {
-                    sender.sendMessage(Utils.getErrorMessageFormat("§lUsage: §c/reload <config/holograms>"));
-                }
+                BTEPlotSystem.getPlugin().saveConfig();
+                BTEPlotSystem.getPlugin().reloadConfig();
+                sender.sendMessage(Utils.getInfoMessageFormat("Successfully reloaded config!"));
+
+                BTEPlotSystem.getHolograms().forEach(HolographicDisplay::updateLeaderboard);
+
+                sender.sendMessage(Utils.getInfoMessageFormat("Successfully reloaded holograms!"));
             } catch (Exception ex) {
                 sender.sendMessage(Utils.getErrorMessageFormat("An error occurred while reloading!"));
                 Bukkit.getLogger().log(Level.SEVERE, "An error occurred while reloading!", ex);
