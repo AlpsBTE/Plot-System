3659d85b06d7ca0c9d3cf91a8fa15682b06c40af
R3tuxn <jonas.limpl@hak-bregenz.at>
2021-04-14 23:37:21 +0200

    Fixed bug when informing players about unfinished plots

--- src/main/java/io/github/BTEPlotSystem/core/EventListener.java
+++ src/main/java/io/github/BTEPlotSystem/core/EventListener.java
@@ -104,8 +104,10 @@ public class EventListener extends SpecialBlocks implements Listener {
 
         // Informing player about unfinished plots
         try {
-            List<Plot> plots = PlotManager.getPlots(new Builder(event.getPlayer().getUniqueId()),Status.unfinished);
-            PlotHandler.sendUnfinishedPlotReminderMessage(plots, event.getPlayer());
+            List<Plot> plots = PlotManager.getPlots(new Builder(event.getPlayer().getUniqueId()), Status.unfinished);
+            if(plots.size() >= 1) {
+                PlotHandler.sendUnfinishedPlotReminderMessage(plots, event.getPlayer());
+            }
         } catch (Exception ex){
             Bukkit.getLogger().log(Level.SEVERE,"An error occurred while trying to inform the player about his unfinished plots!", ex);
         }
