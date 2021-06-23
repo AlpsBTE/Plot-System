758c21585fd9afbad8d2814b79b002509c42f1c7
Leon GRABER <leon.graber@hak-bregenz.at>
2021-06-23 08:14:06 +0200

    implemented get plot members for PlotMemberMenu

--- src/main/java/io/github/BTEPlotSystem/core/menus/PlotMemberMenu.java
+++ src/main/java/io/github/BTEPlotSystem/core/menus/PlotMemberMenu.java
@@ -1,19 +1,29 @@
 package github.BTEPlotSystem.core.menus;
 
+import github.BTEPlotSystem.core.system.Builder;
 import github.BTEPlotSystem.core.system.plot.Plot;
 import github.BTEPlotSystem.utils.ItemBuilder;
 import github.BTEPlotSystem.utils.LoreBuilder;
 import github.BTEPlotSystem.utils.Utils;
+import jdk.internal.jline.internal.Log;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.ipvp.canvas.mask.BinaryMask;
 import org.ipvp.canvas.mask.Mask;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+
 public class PlotMemberMenu extends AbstractMenu {
+
+    private Plot plot;
 
     public PlotMemberMenu(Plot plot, Player menuPlayer) {
         super(3, "Manage Members | Plot # " + plot.getID(), menuPlayer);
+        this.plot = plot;
         Mask mask = BinaryMask.builder(getMenu())
                 .item(new ItemBuilder(Material.STAINED_GLASS_PANE, 1, (byte) 7).setName(" ").build())
                 .pattern("111111111")
@@ -22,13 +32,17 @@ public class PlotMemberMenu extends AbstractMenu {
                 .build();
         mask.apply(getMenu());
 
-        addMenuItems();
+        try {
+            addMenuItems();
+        } catch (SQLException e) {
+            Bukkit.getLogger().log(Level.SEVERE, "SQL Error, while trying to open PlotMemberMenu");
+        }
         setItemClickEvents();
         getMenu().open(menuPlayer.getPlayer());
     }
 
     @Override
-    protected void addMenuItems() {
+    protected void addMenuItems() throws SQLException {
         // Plot Owner Item
         getMenu().getSlot(10)
                 .setItem(new ItemBuilder(Material.SKULL_ITEM, 1)
@@ -46,15 +60,24 @@ public class PlotMemberMenu extends AbstractMenu {
 
         // Member List
         //TODO: Get Plot Members Method
+        List<Builder> builders = plot.getPlotMembers();
         for (int i = 12; i < 15; i++) {
-            // TODO: Check if there is a member on this position
-            getMenu().getSlot(i)
-                    .setItem(new ItemBuilder(Material.STAINED_GLASS_PANE, 1, (byte) 13).setName("§2Empty Member Slot").build());
+            if (builders.size() >= (i-11)) {
+                Builder builder = builders.get(i-11);
+                getMenu().getSlot(i)
+                        .setItem(new ItemBuilder(Utils.getPlayerHead(builder.getUUID()))
+                                .setName("§2" + builder.getName() + " - Member")
+                                .build());
+            } else {
+                getMenu().getSlot(i)
+                        .setItem(new ItemBuilder(Material.STAINED_GLASS_PANE, 1, (byte) 13).setName("§2Empty Member Slot").build());
+            }
         }
     }
 
     @Override
     protected void setItemClickEvents() {
+        // Add Member Button
         getMenu().getSlot(16).setClickHandler((clickPlayer, clickInformation) -> {
             clickPlayer.closeInventory();
             // TODO: Open Anvil UI
