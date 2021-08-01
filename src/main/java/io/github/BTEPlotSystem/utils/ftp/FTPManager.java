385069160b4cb2cc6f1a07b343ac66a67f8559d1
LordTuxn <r3tuxn@gmail.com>
2021-08-01 21:28:43 +0200

    Finished reworking FTPManager

--- src/main/java/io/github/BTEPlotSystem/utils/ftp/FTPManager.java
+++ src/main/java/io/github/BTEPlotSystem/utils/ftp/FTPManager.java
@@ -1,6 +1,5 @@
-package github.BTEPlotSystem.utils;
+package github.BTEPlotSystem.utils.ftp;
 
-import github.BTEPlotSystem.core.system.plot.Plot;
 import org.apache.commons.vfs2.*;
 import org.apache.commons.vfs2.provider.sftp.SftpFileSystemConfigBuilder;
 import org.bukkit.Bukkit;
@@ -10,31 +9,40 @@ import java.util.logging.Level;
 
 public class FTPManager {
 
-    public static String getFTPUrl(Utils.Server server, Plot plot) {
+    private static FileSystemManager fileManager;
+    private static FileSystemOptions fileOptions;
+
+    static {
+        try {
+            fileManager = VFS.getManager();
+
+            fileOptions = new FileSystemOptions();
+            SftpFileSystemConfigBuilder.getInstance().setStrictHostKeyChecking(fileOptions, "no");
+            SftpFileSystemConfigBuilder.getInstance().setUserDirIsRoot(fileOptions, false);
+        } catch (FileSystemException ex) {
+            Bukkit.getLogger().log(Level.SEVERE, "Exception found with FileSystemManager!", ex);
+        }
+    }
+
+    public static String getFTPUrl(Server server, int cityID) {
         return String.format("%sftp://%s:%s@%s:%d/%s%s/",
-                server.ftpConfiguration.secureFTP ? "s" : "",
-                server.ftpConfiguration.username,
-                server.ftpConfiguration.password,
-                server.ftpConfiguration.address,
-                server.ftpConfiguration.port,
-                server.finishedSchematicPath,
-                plot.getCity().getID()
+                server.getFTPConfiguration().getPort() == 22 ? "s" : "",
+                server.getFTPConfiguration().getUsername(),
+                server.getFTPConfiguration().getPassword(),
+                server.getFTPConfiguration().getAddress(),
+                server.getFTPConfiguration().getPort(),
+                server.getSchematicPath(),
+                cityID
         );
     }
 
-    public static void sendFileFTP(String ftpURL, File schematic) {
+    public static void uploadSchematic(String ftpURL, File schematic) {
         try {
-            FileSystemManager fileManager = VFS.getManager();
-
-            FileSystemOptions options = new FileSystemOptions();
-            SftpFileSystemConfigBuilder.getInstance().setStrictHostKeyChecking(options, "no");
-            SftpFileSystemConfigBuilder.getInstance().setUserDirIsRoot(options, false);
-
             // Get local schematic
             FileObject localSchematic = fileManager.toFileObject(schematic);
 
             // Get remote path and create missing directories
-            FileObject remote = fileManager.resolveFile(ftpURL, options);
+            FileObject remote = fileManager.resolveFile(ftpURL, fileOptions);
             remote.createFolder();
 
             // Create remote schematic and write to it
@@ -45,6 +53,27 @@ public class FTPManager {
             remoteSchematic.close();
 
             Bukkit.getLogger().log(Level.INFO, "File " + schematic.getName() + " uploaded successfully!");
+        } catch (FileSystemException ex) {
+            Bukkit.getLogger().log(Level.SEVERE, "Exception found with FileSystemManager!", ex);
+        }
+    }
+
+    public static void downloadSchematic(String ftpURL, File schematic) {
+        try {
+            // Get local schematic
+            FileObject localSchematic = fileManager.toFileObject(schematic);
+
+            // Get remote path
+            FileObject remote = fileManager.resolveFile(ftpURL, fileOptions);
+
+            // Get remote schematic and write it to local file
+            FileObject remoteSchematic = remote.resolveFile(schematic.getName());
+            localSchematic.copyFrom(remoteSchematic, Selectors.SELECT_SELF);
+
+            localSchematic.close();
+            remoteSchematic.close();
+
+            Bukkit.getLogger().log(Level.INFO, "File " + schematic.getName() + " downloaded successfully!");
         } catch (FileSystemException ex) {
             Bukkit.getLogger().log(Level.SEVERE, "Exception found with FileSystemManager!", ex);
         }
