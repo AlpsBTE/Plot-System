1ffae0ded4b1e6c7c4f5c43cd4e8ea143a109a42
LordTuxn <r3tuxn@gmail.com>
2021-08-03 21:00:12 +0200

    Reworked Country.java

--- src/main/java/io/github/BTEPlotSystem/core/system/Country.java
+++ src/main/java/io/github/BTEPlotSystem/core/system/Country.java
@@ -1,29 +1,45 @@
 package github.BTEPlotSystem.core.system;
 
-import github.BTEPlotSystem.BTEPlotSystem;
-import github.BTEPlotSystem.core.config.ConfigPaths;
+import github.BTEPlotSystem.core.database.DatabaseConnection;
+import github.BTEPlotSystem.core.database.builder.StatementBuilder;
 import github.BTEPlotSystem.utils.Utils;
-import github.BTEPlotSystem.utils.ftp.Server;
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.inventory.ItemStack;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.logging.Level;
 
 public class Country {
-    private final static List<Country> countries = new ArrayList<>();
 
-    private final String name;
-    private final ItemStack head;
-    private final Server server;
+    private final int ID;
+    private int serverID;
 
-    public Country(String name, String headID, Server server) {
-        this.name = name;
-        this.head = Utils.getItemHead(headID);
-        this.server = server;
+    private String name;
+    private String headID;
+
+    public Country(int ID) throws SQLException {
+        this.ID = ID;
+
+        String sql = "SELECT * FROM plotsystem_countries WHERE id = ?";
+        ResultSet rs = DatabaseConnection.query(new StatementBuilder(sql)
+                .setInt(this.ID).build());
+
+        if (!rs.wasNull()) {
+            this.serverID = rs.getInt("server_id");
+            this.name = rs.getString("name");
+            this.headID = rs.getString("head_id");
+        }
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public int getServer_id() {
+        return serverID;
     }
 
     public String getName() {
@@ -31,53 +47,22 @@ public class Country {
     }
 
     public ItemStack getHead() {
-        return head;
-    }
-
-    public Server getServer() {
-        return server;
-    }
-
-    public static void init() {
-        if (!Server.getServers().isEmpty()) {
-            FileConfiguration config = BTEPlotSystem.getPlugin().getConfig();
-            Set<String> countrySection = config.getConfigurationSection(ConfigPaths.COUNTRIES).getKeys(false);
-
-            countries.clear();
-            for (String countryKey : countrySection) {
-                String configPath = ConfigPaths.COUNTRIES + countryKey;
-                if(countries.stream().anyMatch(s -> s.name.equals(countryKey))) {
-                    Bukkit.getLogger().log(Level.WARNING, "Country (" + countryKey + ") is already registered! Please check your config!");
-                    continue;
-                }
-
-                String headID;
-                Server server;
-                try {
-                    headID = config.getString(configPath + ConfigPaths.COUNTRIES_HEAD_ID);
-
-                    String serverName = config.getString(configPath + ConfigPaths.COUNTRIES_SERVER);
-                    server = Server.getServers().stream().filter(c -> c.getName().equals(serverName)).findFirst().orElse(null);
-                    if (server == null) {
-                        Bukkit.getLogger().log(Level.WARNING, "Could not initialize country (" + countryKey + ")! Please check your config!");
-                        Bukkit.getLogger().log(Level.WARNING, "Server (" + serverName + ") in country (" + countryKey + ") is not registered!");
-                        continue;
-                    }
-                } catch (Exception ex) {
-                    Bukkit.getLogger().log(Level.WARNING, "Could not initialize country (" + countryKey + ")! Please check your config!");
-                    continue;
-                }
-
-                countries.add(new Country(countryKey, headID, server));
-            }
-
-            if (countries.isEmpty()) {
-                Bukkit.getLogger().log(Level.WARNING, "No country could be registered! Please check your config!");
-            }
-        }
+        return Utils.getItemHead(headID);
     }
 
     public static List<Country> getCountries() {
-        return countries;
+        try {
+            String sql = "SELECT id FROM plotsystem_countries ORDER BY server_id";
+            ResultSet rs = DatabaseConnection.query(new StatementBuilder(sql).build());
+
+            List<Country> countries = new ArrayList<>();
+            while (rs.next()) {
+                countries.add(new Country(rs.getInt(1)));
+            }
+            return countries;
+        } catch (SQLException ex) {
+            Bukkit.getLogger().log(Level.SEVERE, "A SQL error occurred!", ex);
+        }
+        return new ArrayList<>();
     }
 }
