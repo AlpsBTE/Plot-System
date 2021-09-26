5e0d2947d2115d8dcc906786cfdf9e0452749e8d
LordTuxn <r3tuxn@gmail.com>
2021-09-26 18:19:56 +0200

    Added missing license comments

--- src/main/java/com/alpsbte/plotsystem/core/system/plot/generator/AbstractPlotGenerator.java
+++ src/main/java/com/alpsbte/plotsystem/core/system/plot/generator/AbstractPlotGenerator.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ *  Copyright © 2021, Alps BTE <bte.atchli@gmail.com>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
 package com.alpsbte.plotsystem.core.system.plot.generator;
 
 import com.alpsbte.plotsystem.PlotSystem;
@@ -174,6 +198,8 @@ public abstract class AbstractPlotGenerator {
         plot.getPlotWorld().setTime(6000);
 
         // Configure multiverse world
+        mvWorld.setSpawnLocation(PlotHandler.getPlotSpawnPoint(getPlot()));
+        mvWorld.setAdjustSpawn(false);
         mvWorld.setAllowFlight(true);
         mvWorld.setGameMode(GameMode.CREATIVE);
         mvWorld.setEnableWeather(false);
@@ -182,7 +208,6 @@ public abstract class AbstractPlotGenerator {
         mvWorld.setAllowMonsterSpawn(false);
         mvWorld.setAutoLoad(false);
         mvWorld.setKeepSpawnInMemory(false);
-        mvWorld.setSpawnLocation(PlotHandler.getPlotSpawnPoint(getPlot()));
 
         return CompletableFuture.completedFuture(true);
     }
