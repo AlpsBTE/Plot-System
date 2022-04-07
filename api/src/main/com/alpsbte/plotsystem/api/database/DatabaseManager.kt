bc0187a428c751cd990a2d2d353ca3a98f6ba271
LordTuxn <r3tuxn@gmail.com>
2022-04-07 13:03:24 +0200

    implemented connection pooling

--- api/src/main/com/alpsbte/plotsystem/api/database/DatabaseManager.kt
+++ api/src/main/com/alpsbte/plotsystem/api/database/DatabaseManager.kt
@@ -1,13 +1,31 @@
 package com.alpsbte.plotsystem.api.database
 
 import org.ktorm.database.Database
+import org.mariadb.jdbc.MariaDbDataSource
+import java.io.IOException
+import javax.sql.DataSource
 
-open class DatabaseManager(url: String, dbName: String, user: String, password: String) {
+
+open class DatabaseManager(var url: String, var dbName: String, var user: String, var password: String) {
     companion object {
         @JvmStatic lateinit var connection: Database
     }
 
     init {
-        connection = Database.connect(url + dbName, "org.mariadb.jdbc.Driver", user, password)
+        val datasource = getMariaDBDataSource()
+        connection = if (datasource != null) Database.connect(datasource) else Database.connect(url + dbName, "org.mariadb.jdbc.Driver", user, password)
+    }
+
+    private fun getMariaDBDataSource(): DataSource? {
+        var mariaDBDS: MariaDbDataSource? = null
+        try {
+            mariaDBDS = MariaDbDataSource()
+            mariaDBDS.setUrl(url + dbName)
+            mariaDBDS.user = this.user
+            mariaDBDS.setPassword(password)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return mariaDBDS
     }
 }
