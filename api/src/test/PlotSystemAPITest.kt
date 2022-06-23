59c90a01928d560786823b2d536fd3f4240330bb
LordTuxn <r3tuxn@gmail.com>
2022-06-23 22:48:35 +0200

    update tests

--- api/src/test/PlotSystemAPITest.kt
+++ api/src/test/PlotSystemAPITest.kt
@@ -1,4 +1,10 @@
-import com.alpsbte.plotsystem.api.PlotSystemAPI
+import com.alpsbte.plotsystem.api.database.DatabaseManager
+import com.alpsbte.plotsystem.api.database.repositories.CityProjectRepositoryMySQL
+import com.alpsbte.plotsystem.api.database.repositories.PlotRepositoryMySQL
+import com.alpsbte.plotsystem.api.enums.PlotStatus
+import com.alpsbte.plotsystem.api.http.HTTPManager
+import com.alpsbte.plotsystem.api.http.repositories.CityProjectRepositoryHTTP
+import com.alpsbte.plotsystem.api.http.repositories.PlotRepositoryHTTP
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import org.junit.jupiter.api.BeforeAll
@@ -6,60 +12,60 @@ import org.junit.jupiter.api.Test
 
 class PlotSystemAPITest {
     companion object {
-        private lateinit var plotAPI: PlotSystemAPI
+        private var config: Config = Gson().fromJson(PlotSystemAPITest::class.java.getResourceAsStream("config.json")?.bufferedReader(), Config::class.java);
 
-        @JvmStatic
         @BeforeAll
         fun setUp() {
-            val config: Config = Gson().fromJson(PlotSystemAPITest::class.java.getResourceAsStream("config.json")?.bufferedReader(), Config::class.java)
-            plotAPI = PlotSystemAPI(config.apiUrl, config.apiKey)
+            DatabaseManager(
+                config.apiUrl,
+                config.dbName,
+                config.dbUser,
+                config.dbPassword
+            )
+
+            val plotLength = PlotRepositoryMySQL().getPlots().size
+
+            for (i in 33 until plotLength) {
+                val cityProject = CityProjectRepositoryMySQL().getCityProject(1)
+
+                if (cityProject != null) {
+                    println("City Project with the ID: ${cityProject.cityId} has the name: ${cityProject.name}")
+                    Thread.sleep(500)
+                }
+            }
         }
+
+        @Test
+        fun testHttpRequest() {
+            HTTPManager(config.apiUrl, config.apiKey)
+            val cityProject = CityProjectRepositoryHTTP().getCityProjects()
+            println(cityProject[1])
+        }
+
+        @Test
+        fun testHttpRequest2() {
+            HTTPManager(config.apiUrl, config.apiKey)
+            val cityProject = CityProjectRepositoryHTTP().getCityProject(1)
+            println(cityProject)
+        }
+
+        @Test
+        fun testHttpRequest3() {
+            HTTPManager(config.apiUrl, config.apiKey)
+            val plots = PlotRepositoryHTTP().getPlots(PlotStatus.COMPLETED, false, 10)
+
+            for (plot in plots) {
+                println(plot)
+            }
+        }
+
+        data class Config(
+            @SerializedName("api-url") val apiUrl: String,
+            @SerializedName("api-key") val apiKey: String,
+            @SerializedName("db-url") val dbURL: String,
+            @SerializedName("db-name") val dbName: String,
+            @SerializedName("db-user") val dbUser: String,
+            @SerializedName("db-password") val dbPassword: String
+        )
     }
-
-    @Test
-    fun testGetPlotsRoute() {
-        printListSize(plotAPI.getPlots())
-    }
-
-    @Test
-    fun testGetPlotsWithStatusRoute() {
-        printListSize(plotAPI.getPlots(PlotSystemAPI.Status.UNCLAIMED))
-        printListSize(plotAPI.getPlots(PlotSystemAPI.Status.UNFINISHED))
-        printListSize(plotAPI.getPlots(PlotSystemAPI.Status.UNREVIEWED))
-        printListSize(plotAPI.getPlots(PlotSystemAPI.Status.COMPLETED))
-    }
-
-    @Test
-    fun testGetPlotsWithStatusAndLimitRoute() {
-        printListSize(plotAPI.getPlots(PlotSystemAPI.Status.UNCLAIMED, -1))
-        printListSize(plotAPI.getPlots(PlotSystemAPI.Status.UNCLAIMED, 0))
-        printListSize(plotAPI.getPlots(PlotSystemAPI.Status.UNCLAIMED, 1))
-
-        printListSize(plotAPI.getPlots(PlotSystemAPI.Status.UNFINISHED, 5))
-        printListSize(plotAPI.getPlots(PlotSystemAPI.Status.UNREVIEWED, 25))
-        printListSize(plotAPI.getPlots(PlotSystemAPI.Status.COMPLETED, 50))
-    }
-
-    @Test
-    fun testGetPlotsWithStatusAndLimitAndPastedRoute() {
-        printListSize(plotAPI.getPlots(PlotSystemAPI.Status.COMPLETED, 5, true))
-        printListSize(plotAPI.getPlots(PlotSystemAPI.Status.COMPLETED, 10, false))
-        printListSize(plotAPI.getPlots(PlotSystemAPI.Status.UNFINISHED, -1, false))
-    }
-
-    @Test
-    fun testGetCityProjectsRoute() {
-
-    }
-
-    @Test
-    fun testGetFTPConfiguration() {
-
-    }
-
-    private fun printListSize(list: List<Any>) {
-        println("Size: ${list.size}")
-    }
-
-    data class Config(@SerializedName("api-url") val apiUrl: String, @SerializedName("api-key") val apiKey: String)
 }
