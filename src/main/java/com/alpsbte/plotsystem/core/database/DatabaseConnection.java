9ecf1569a5415e6d7c32023e63ad7b2985b4f80d
LordTuxn <r3tuxn@gmail.com>
2023-11-11 14:50:17 +0100

    update plotsystem_plots_tutorial sql create script

--- src/main/java/com/alpsbte/plotsystem/core/database/DatabaseConnection.java
+++ src/main/java/com/alpsbte/plotsystem/core/database/DatabaseConnection.java
@@ -396,16 +396,21 @@ public class DatabaseConnection {
 
                     // Tutorial Plots
                     "CREATE TABLE IF NOT EXISTS `plotsystem_plots_tutorial` (" +
-                            " `id`            int(11) NOT NULL AUTO_INCREMENT," +
-                            " `owner_uuid`    varchar(36) NOT NULL," +
-                            " `tutorial_id`   int(11) NOT NULL," +
-                            " `stage`         int(11) NOT NULL DEFAULT 1," +
-                            " `is_completed`  tinyint NOT NULL," +
-                            " `last_activity` datetime NOT NULL," +
-                            "PRIMARY KEY (`id`)," +
-                            "KEY `FK_142` (`owner_uuid`)," +
-                            "CONSTRAINT `FK_12` FOREIGN KEY `FK_142` (`owner_uuid`) REFERENCES `plotsystem_builders` (`uuid`)" +
-                            ");"
+                            "`id` INT(11) NOT NULL AUTO_INCREMENT," +
+                            "`player_uuid` VARCHAR(36) NOT NULL COLLATE 'utf8mb4_general_ci'," +
+                            "`tutorial_id` INT(11) NOT NULL," +
+                            "`stage_id` INT(11) NOT NULL DEFAULT '0'," +
+                            "`is_completed` TINYINT(4) NOT NULL DEFAULT '0'," +
+                            "`create_date` DATETIME NOT NULL DEFAULT current_timestamp()," +
+                            "`last_stage_complete_date` DATETIME NULL DEFAULT NULL," +
+                            "`complete_date` DATETIME NULL DEFAULT NULL," +
+                            "PRIMARY KEY (`id`) USING BTREE," +
+                            "INDEX `FK_142` (`player_uuid`) USING BTREE," +
+                            "CONSTRAINT `FK_12` FOREIGN KEY (`player_uuid`) REFERENCES `plotsystem_builders` (`uuid`) ON UPDATE RESTRICT ON DELETE RESTRICT" +
+                            ")" +
+                            "COLLATE='utf8mb4_general_ci'" +
+                            "ENGINE=InnoDB" +
+                            ";"
             );
         }
     }
