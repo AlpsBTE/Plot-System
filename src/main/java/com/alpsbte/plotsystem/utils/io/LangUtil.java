f16e9e77d33971b05b9f099b9c27291735960248
Zoriot <zoriot@proton.me>
2025-06-09 16:29:00 +0200

    🔊🩹 Fix exception log issue & correct language file versions
    Signed-off-by: Zoriot <zoriot@proton.me>

--- src/main/java/com/alpsbte/plotsystem/utils/io/LangUtil.java
+++ src/main/java/com/alpsbte/plotsystem/utils/io/LangUtil.java
@@ -41,14 +41,14 @@ public class LangUtil extends LanguageUtil {
         if (langUtilInstance != null) return;
         Plugin plugin = PlotSystem.getPlugin();
         LangLibAPI.register(plugin, new LanguageFile[]{
-                new LanguageFile(plugin, 2.5, Language.en_GB),
-                new LanguageFile(plugin, 2.5, Language.de_DE, "de_AT", "de_CH"),
-                new LanguageFile(plugin, 2.5, Language.fr_FR, "fr_CA"),
-                new LanguageFile(plugin, 2.5, Language.pt_PT, "pt_BR"),
-                new LanguageFile(plugin, 2.5, Language.ko_KR),
-                new LanguageFile(plugin, 2.5, Language.ru_RU, "ba_RU", "tt_RU"),
-                new LanguageFile(plugin, 2.5, Language.zh_CN),
-                new LanguageFile(plugin, 2.5, Language.zh_TW, "zh_HK"),
+                new LanguageFile(plugin, 2.2, Language.en_GB),
+                new LanguageFile(plugin, 2.2, Language.de_DE, "de_AT", "de_CH"),
+                new LanguageFile(plugin, 2.3, Language.fr_FR, "fr_CA"),
+                new LanguageFile(plugin, 2.2, Language.pt_PT, "pt_BR"),
+                new LanguageFile(plugin, 2.2, Language.ko_KR),
+                new LanguageFile(plugin, 2.2, Language.ru_RU, "ba_RU", "tt_RU"),
+                new LanguageFile(plugin, 2.2, Language.zh_CN),
+                new LanguageFile(plugin, 2.3, Language.zh_TW, "zh_HK"),
                 new LanguageFile(plugin, 1.0, Language.he_IL),
         });
         langUtilInstance = new LangUtil();
