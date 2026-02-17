c80d1e05a4d94fc06db9c2439edacd12798198e9
Zoriot <zoriot@proton.me>
2026-02-17 21:12:32 +0100

    build: 🔖 Release v5.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,7 +65,7 @@ val versionDetails: groovy.lang.Closure<com.palantir.gradle.gitversion.VersionDe
 val details = versionDetails()
 
 group = "com.alpsbte"
-version = "5.0.1" + "-" + details.gitHash + "-SNAPSHOT"
+version = "5.0.1" // + "-" + details.gitHash + "-SNAPSHOT"
 description = "An easy to use building system for the BuildTheEarth project."
 java.sourceCompatibility = JavaVersion.VERSION_21
 
